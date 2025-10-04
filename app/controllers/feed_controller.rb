2b8563c8b119961cdc1b5fc221d084949dfdd3da
André Jun Hirata <andrejhirata@usp.br>
2025-10-03 22:06:24 -0300

    Adicionado barra para pesquisa de usuario

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,4 +2,17 @@ class FeedController < ApplicationController
   def show
     @posts = Post.all
   end
+
+  # [bookmark] editar a query para aceitar outras coisas
+  def search
+    if params[:query].present?
+      query = "%#{params[:query]}%"
+      @users = User.joins(:profile).where(
+        "users.username LIKE :q",
+        q: query
+      )
+    else
+      @users = User.none
+    end
+  end
 end
