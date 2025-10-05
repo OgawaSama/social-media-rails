21ac362299071fd4613a5e9fc843e902e99050f3
OgawaSama <ogawamateus@gmail.com>
2025-10-04 22:27:39 -0300

    Add friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :reactions, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :friendships, dependent: :destroy
   has_one :profile, dependent: :destroy
   after_create :create_user_profile
 
