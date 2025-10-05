f9356bece86bf0f9061490ced3252c46f43f8ca4
guilherme <guilhermesangabriel@gmail.com>
2025-10-05 20:39:09 -0300

    add unit testing for business and business_address

--- test/models/business_test.rb
+++ test/models/business_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 class BusinessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one) # assumes you have a users(:one) fixture
+    @business = Business.new(
+      company_name: "Placeholder Project",
+      cnpj: "49610796001",
+      user: @user
+    )
+  end
+
+  test "should be valid with valid attributes" do
+    assert @business.valid?
+  end
+
+  test "should be invalid without company_name" do
+    @business.company_name = nil
+    assert_not @business.valid?
+    assert_includes @business.errors[:company_name], "can't be blank"
+  end
+
+  test "should be invalid without cnpj" do
+    @business.cnpj = nil
+    assert_not @business.valid?
+    assert_includes @business.errors[:cnpj], "can't be blank"
+  end
+
+  test "should be invalid if cnpj has less than 14 digits" do
+    @business.cnpj = "1234567890"
+    assert_not @business.valid?
+    assert_includes @business.errors[:cnpj], "deve ter 14 números"
+  end
+
+  test "should be invalid if cnpj has more than 14 digits" do
+    @business.cnpj = "99999999999999999999999999"
+    assert_not @business.valid?
+    assert_includes @business.errors[:cnpj], "deve ter 14 números"
+  end
+
+  test "should allow nested business addresses" do
+    @business.business_addresses.build(address: "Rua do abububle, 321")
+    assert_difference "BusinessAddress.count", 1 do
+      @business.save!
+    end
+  end
+
+  test "should destroy associated business_addresses on destroy" do
+    @business.save!
+    @business.business_addresses.create!(address: "Avenida Ceci, 945")
+    assert_difference "BusinessAddress.count", -1 do
+      @business.destroy
+    end
+  end
 end
