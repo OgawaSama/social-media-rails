a531cea5f5b2760463e6efbc6c9c000f42d1adaa
andre-jun <andrejhirata@usp.br>
2025-10-10 21:22:41 -0300

    Add - NavBar and cleanup of Feed

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,6 @@
 pin "application"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.18
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.18
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.300
