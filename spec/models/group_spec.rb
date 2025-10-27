b839301862befab6b1d447d706065100d8fd201c
isabela <belabrendel96@gmail.com>
2025-10-26 21:09:14 -0300

    resolvendo problema do risize nos groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   before(:each) do
+    # Evita que o job de resize rode de verdade durante os testes
+    allow(ResizeImageJob).to receive(:perform_later)
     @group = build(:group)
   end
 
-  # [Bookmark]
-  # edit image validations after changing file restrictions
   it "checks for valid header formats" do
     expect(@group.header).to be_valid
 
     @group.header.attach(
-            io: File.open(Rails.root.join('test', 'fixtures', 'files', 'pdf.pdf')),
-            filename: 'pdf.pdf',
-            content_type: 'application/pdf'
-        )
+      io: File.open(Rails.root.join('test', 'fixtures', 'files', 'pdf.pdf')),
+      filename: 'pdf.pdf',
+      content_type: 'application/pdf'
+    )
     expect(@group).not_to be_valid
   end
 
@@ -22,23 +22,18 @@ RSpec.describe Group, type: :model do
     expect(@group.avatar).to be_valid
 
     @group.avatar.attach(
-            io: File.open(Rails.root.join('test', 'fixtures', 'files', 'pdf.pdf')),
-            filename: 'pdf.pdf',
-            content_type: 'application/pdf'
-        )
+      io: File.open(Rails.root.join('test', 'fixtures', 'files', 'pdf.pdf')),
+      filename: 'pdf.pdf',
+      content_type: 'application/pdf'
+    )
     expect(@group).not_to be_valid
   end
 
   it "checks for valid bio" do
-    # [Bookmark]
-    # no restrictions for now
     expect(true).to be(true)
   end
 
   it "checks for owner" do
-    # [Bookmark]
-    # for now, nothing
-    # change when implement ownership on groups
     expect(true).to be(true)
   end
 end
