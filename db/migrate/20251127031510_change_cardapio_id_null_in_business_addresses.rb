4d0ec40b204ad0173011d7c9096caf42df2ebfde
Davi Moura <mouradavi12@gmail.com>
2025-11-27 04:55:11 +0000

    style: auto-fix rubocop linter errors

--- db/migrate/20251127031510_change_cardapio_id_null_in_business_addresses.rb
+++ db/migrate/20251127031510_change_cardapio_id_null_in_business_addresses.rb
@@ -1,4 +1,4 @@
-class ChangeCardapioIdNullInBusinessAddresses < ActiveRecord::Migration[7.0] # ou a tua versão
+class ChangeCardapioIdNullInBusinessAddresses < ActiveRecord::Migration[8.1]
   def change
     change_column_null :business_addresses, :cardapio_id, true
   end
